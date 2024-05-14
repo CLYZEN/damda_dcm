bc71572db578c79db09aff6e3105d970f193461d
CLYZEN <jimuny01@gmail.com>
2024-05-14 13:06:36 +0900

    Initial Commit

---
+++ dcm/src/test/java/kr/damda/dcm/DcmApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.damda.dcm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
